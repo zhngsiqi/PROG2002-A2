41013eaa095bcb87de4fc29dfc06ac7152048542
zhngsiqi <s.zhang.42@student.scu.edu.au>
2025-09-30 01:12:24 +0800

    build search page and interact with API

--- routes/events.js
+++ routes/events.js
@@ -25,4 +25,50 @@ router.get('/', (req, res) => {
     });
 });
 
+// search events
+router.get('/search', (req, res) => {
+  // obtain query date/location/category
+  const date = req.query.date;
+  const location = req.query.location;
+  const category = req.query.category;
+
+  let sql = `
+    SELECT e.event_id, e.name, e.location, e.start_date, e.end_date, 
+           e.ticket_price, e.status, c.name AS category, o.name AS organisation
+    FROM events e
+    JOIN categories c ON e.category_id = c.category_id
+    JOIN organisations o ON e.organisation_id = o.organisation_id
+    WHERE e.status != 'suspended'
+  `;
+
+  // contact sql and push parameters
+  let params = [];
+
+  // date query to search event start date
+  if (date) {
+    sql += ' AND DATE(e.start_date) >= ?';
+    params.push(date);
+  }
+  // location query to search event location (similar search)
+  if (location) {
+    sql += ' AND e.location LIKE ?';
+    params.push(`%${location}%`);
+  }
+  // category search to search category name
+  if (category) {
+    sql += ' AND c.name = ?';
+    params.push(category);
+  }
+
+  conn.promise().query(sql, params)
+    .then(([rows]) => res.json(rows))  // return json to frontend
+    .catch(err => {
+      // console and handle error
+      console.log(err.message)
+      res.status(500).json({
+        error: "Server error"
+      })
+    });
+});
+
 module.exports = router;
